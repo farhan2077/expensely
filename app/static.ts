33fac76e9848c2e723a52e45f8c1a1e1deb06036
farhan2077 <farhanbinamin@gmail.com>
2024-06-26 11:44:09 +0600

    fix: updated base url

--- app/static.ts
+++ app/static.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://expensely.vercel.app/";
+export const BASE_URL = "https://expensely-alpha.vercel.app"; // dont't include `/` at the end
 export const SEO_TITLE = "Expensely";
 export const SEO_DESCRIPTION = "Manage shared expenses with Expensely";
 export const SEO_KEYWORDS = "expense management";
