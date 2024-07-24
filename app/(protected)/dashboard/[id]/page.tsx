cbadd0d2871be264f72b2a9367ecf0815e823b63
farhan2077 <farhanbinamin@gmail.com>
2024-07-25 05:24:29 +0600

    new: add individual member's aggregated daily data

--- app/(protected)/dashboard/[id]/page.tsx
+++ app/(protected)/dashboard/[id]/page.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
-import { DollarSign, CookingPot, Utensils } from "lucide-react";
+import {
+  DollarSign,
+  CookingPot,
+  Utensils,
+  UtensilsCrossed,
+  Banknote,
+} from "lucide-react";
 
 import { getGroupInfo } from "@/app/actions/group";
 import {
@@ -14,15 +20,14 @@ import {
 import {
   getDailyActivities,
   getDailyActivitiesDates,
-  type DailyActivityOutput,
-  type DailyActivityDateOutput,
+  type DailyActivityOutputData,
+  type DailyActivityDateOutputData,
 } from "@/app/actions/daily-activity";
 import {
   columns,
   type DailyActivityRow,
 } from "@/components/tables/daily-activities/columns";
 import AddDailyActivityButton from "@/app/(protected)/dashboard/[id]/AddDailyActivityButton";
-
 import InfoCard from "@/components/InfoCard";
 import { DataTable } from "@/components/tables/daily-activities/data-table";
 import MonthPicker from "@/app/(protected)/dashboard/[id]/MonthPicker";
@@ -34,9 +39,63 @@ export const metadata: Metadata = {
   description: "Welcome to your expensely dashboard",
 };
 
+type MemberTotal = {
+  userId: string;
+  name: string;
+  totalMeal: number;
+  totalGrocery: number;
+};
+
+function calculateMembersTotals(
+  data: DailyActivityOutputData[]
+): MemberTotal[] {
+  const output = data.reduce((acc: MemberTotal[], curr) => {
+    const existingUser = acc.find((obj) => obj.userId === curr.userId);
+
+    if (existingUser) {
+      existingUser.totalMeal += curr.meal;
+      existingUser.totalGrocery += curr.grocery;
+    } else {
+      acc.push({
+        userId: curr.userId,
+        name: curr.user.name,
+        totalMeal: curr.meal,
+        totalGrocery: curr.grocery,
+      });
+    }
+
+    return acc;
+  }, []);
+
+  return output;
+}
+
+function calculateTotals(data: DailyActivityOutputData[]): {
+  totalMeal: number;
+  totalGrocery: number;
+  avgMealRate: number;
+} {
+  let totalMeal = 0;
+  let totalGrocery = 0;
+  let avgMealRate = 0;
+
+  data.forEach((item) => {
+    totalMeal += item.meal;
+    totalGrocery += item.grocery;
+  });
+
+  if (totalMeal === 0) {
+    avgMealRate = 0;
+  } else {
+    avgMealRate = Number((totalGrocery / totalMeal).toFixed(2));
+  }
+
+  return { totalMeal, totalGrocery, avgMealRate };
+}
+
 //* 1a. transform the data to be used in data table
 function transformDailyActivities(
-  data: DailyActivityOutput[]
+  data: DailyActivityOutputData[]
 ): DailyActivityRow[] {
   const output = data.reduce((acc: DailyActivityRow[], curr) => {
     const existingGroup = acc.find((group) => group.date === curr.date);
@@ -58,9 +117,9 @@ function transformDailyActivities(
 
 //* 1b. transform the data to be used in month picker and to show disabled dates while adding data
 function transformDailyActivitiesDates(
-  data: DailyActivityDateOutput[]
-): DailyActivityDateOutput[] {
-  const output = data.reduce((acc: DailyActivityDateOutput[], curr) => {
+  data: DailyActivityDateOutputData[]
+): DailyActivityDateOutputData[] {
+  const output = data.reduce((acc: DailyActivityDateOutputData[], curr) => {
     const existingGroup = acc.find((group) => group.date === curr.date);
 
     if (!existingGroup) {
@@ -76,10 +135,9 @@ function transformDailyActivitiesDates(
 }
 
 //* 2. sort both daily activities and daily activities dates
-function sortByDate<T extends DailyActivityRow[] | DailyActivityDateOutput[]>(
-  data: T,
-  type: "asc" | "desc"
-): T {
+function sortByDate<
+  T extends DailyActivityRow[] | DailyActivityDateOutputData[],
+>(data: T, type: "asc" | "desc"): T {
   // asc = 1 2 3 4 5
   // desc = 5 4 3 2 1
 
@@ -93,32 +151,6 @@ function sortByDate<T extends DailyActivityRow[] | DailyActivityDateOutput[]>(
   return data.sort(
     (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
   ) as T;
-}
-
-//* 3a. calculation from transformed activities
-function calculateTotals(data: DailyActivityRow[]): {
-  totalMeal: number;
-  totalGrocery: number;
-  avgMealRate: number;
-} {
-  let totalMeal = 0;
-  let totalGrocery = 0;
-  let avgMealRate = 0;
-
-  data.forEach((day) => {
-    day.rest.forEach((item) => {
-      totalMeal += item.meal;
-      totalGrocery += item.grocery;
-    });
-  });
-
-  if (totalMeal === 0) {
-    avgMealRate = 0;
-  } else {
-    avgMealRate = Number((totalGrocery / totalMeal).toFixed(2));
-  }
-
-  return { totalMeal, totalGrocery, avgMealRate };
 }
 
 type UniqueMonthInput = {
@@ -145,7 +177,7 @@ function extractUniqueMonths(data: UniqueMonthInput[]): UniqueMonthOutput[] {
 }
 
 //* 3. from transformed daily activites dates, get last 2 months' dates (on which day there we activities)
-function getLastTwoMonthsDates(array: DailyActivityDateOutput[]): string[] {
+function getLastTwoMonthsDates(array: DailyActivityDateOutputData[]): string[] {
   const today = new Date();
   const startOfLastMonth = startOfMonth(subMonths(today, 1));
   const endOfCurrentMonth = endOfMonth(today);
@@ -163,21 +195,20 @@ function getLastTwoMonthsDates(array: DailyActivityDateOutput[]): string[] {
 
 type PageProps = {
   params: { id: string };
-  searchParams: { [key: string]: string | undefined };
+  searchParams: {
+    from?: string;
+    to?: string;
+  };
 };
 
 async function Page({ params, searchParams }: PageProps) {
   const groupId = params.id;
-  const fromSP = searchParams.from;
-  const toSP = searchParams.to;
+  const fromSP = searchParams?.from || "";
+  const toSP = searchParams?.to || "";
 
   const { user } = await validateSession();
   const groupInfo = await getGroupInfo(groupId);
-  const dailyActivities = await getDailyActivities(
-    groupId,
-    !fromSP ? "" : fromSP,
-    !toSP ? "" : toSP
-  );
+  const dailyActivities = await getDailyActivities(groupId, fromSP, toSP);
   const dailyActivitiesMonths = await getDailyActivitiesDates(groupId);
 
   if (
@@ -192,26 +223,31 @@ async function Page({ params, searchParams }: PageProps) {
   }
 
   // daily activities
+  const totals = calculateTotals(dailyActivities.data);
+  const groupMembersTotals = calculateMembersTotals(dailyActivities.data);
+
   const transformedActivities = transformDailyActivities(dailyActivities.data);
   const sortedActivities = sortByDate(transformedActivities, "desc");
-  const totals = calculateTotals(transformedActivities);
 
   // months
   const transformedMonths = transformDailyActivitiesDates(
     dailyActivitiesMonths.data
   );
+  const disabledDates = getLastTwoMonthsDates(transformedMonths);
   const sortedMonths = sortByDate(transformedMonths, "desc");
   const months = extractUniqueMonths(sortedMonths);
-  const disabledDates = getLastTwoMonthsDates(transformedMonths);
 
   return (
-    <main>
-      <h1 className="text-2xl font-semibold tracking-tight">Dashboard</h1>
-      <p className="text-muted-foreground">
-        Take a look at what&apos;s happening in {groupInfo.data.groupInfo.name}
-      </p>
-      <hr className="my-4 text-muted-foreground" />
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 lg:grid-cols-6">
+    <div className="grid gap-4">
+      <section>
+        <h1 className="text-2xl font-semibold tracking-tight">Dashboard</h1>
+        <p className="text-muted-foreground">
+          Take a look at what&apos;s happening in{" "}
+          {groupInfo.data.groupInfo.name}
+        </p>
+      </section>
+      <hr className="text-muted-foreground" />
+      <section className="grid w-full grid-cols-1 gap-4 sm:grid-cols-3 lg:grid-cols-6">
         <InfoCard
           title="Total bazar costs"
           body={totals.totalGrocery}
@@ -230,9 +266,37 @@ async function Page({ params, searchParams }: PageProps) {
           bodyType="number"
           icon={Utensils}
         />
-      </div>
-      <div className="my-4">
-        <div className="flex items-center justify-end gap-4">
+      </section>
+      <section>
+        <h2 className="my-4 text-lg font-bold leading-none">
+          Everyone&apos;s grocery costs and meals so far
+        </h2>
+        <div className="flex divide-x-2 divide-border">
+          {groupMembersTotals.map((member) => {
+            return (
+              <div key={member.userId} className="px-8 first:pl-0 last:pr-0">
+                <p className="text-sm font-medium">{member.name}</p>
+                <div className="mt-1">
+                  <div className="flex items-center gap-2">
+                    <Banknote className="h-4 w-4" />
+                    <span className="text-sm tabular-nums">
+                      {member.totalGrocery} BDT
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <UtensilsCrossed className="h-4 w-4" />
+                    <span className="text-sm tabular-nums">
+                      {member.totalMeal}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+      <div>
+        <div className="flex flex-col items-start justify-end gap-4 sm:flex-row">
           {isEmpty(months) ? null : <MonthPicker months={months} />}
           {isEqual(user.id, groupInfo.data.groupInfo.ownerId) ? (
             <AddDailyActivityButton
@@ -243,7 +307,7 @@ async function Page({ params, searchParams }: PageProps) {
         </div>
       </div>
       <DataTable data={sortedActivities} columns={columns} />
-    </main>
+    </div>
   );
 }
 
