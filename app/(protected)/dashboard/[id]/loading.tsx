d0d6b896efcb62b02d3ce8bd2fcf0e74290972ff
farhan2077 <farhanbinamin@gmail.com>
2024-08-01 20:27:54 +0600

    fix: loading ui color for dark mode

--- app/(protected)/dashboard/[id]/loading.tsx
+++ app/(protected)/dashboard/[id]/loading.tsx
@@ -3,14 +3,14 @@
 export default function Loading() {
   return (
     <main className="mt-8 animate-pulse">
-      <div className="h-6 w-1/4 rounded bg-gray-200"></div>
-      <div className="mt-2 h-4 w-1/2 rounded bg-gray-200"></div>
+      <div className="h-6 w-1/4 rounded bg-muted"></div>
+      <div className="mt-2 h-4 w-1/2 rounded bg-muted"></div>
       <hr className="my-4" />
       <div>
-        <div className="mt-4 h-4 w-3/4 rounded bg-gray-200"></div>
-        <div className="mt-2 h-4 w-2/3 rounded bg-gray-200"></div>
-        <div className="mt-2 h-4 w-full rounded bg-gray-200"></div>
-        <div className="mt-2 h-4 w-1/2 rounded bg-gray-200"></div>
+        <div className="mt-4 h-4 w-3/4 rounded bg-muted"></div>
+        <div className="mt-2 h-4 w-2/3 rounded bg-muted"></div>
+        <div className="mt-2 h-4 w-full rounded bg-muted"></div>
+        <div className="mt-2 h-4 w-1/2 rounded bg-muted"></div>
       </div>
     </main>
   );
--- app/(protected)/dashboard/[id]/utilities/loading.tsx
+++ app/(protected)/dashboard/[id]/utilities/loading.tsx
@@ -4,23 +4,23 @@ export default function Loading() {
   return (
     <main className="mt-8 flex animate-pulse gap-8">
       <div className="w-64 p-4">
-        <div className="mb-2 h-4 rounded bg-gray-200"></div>
-        <div className="h-3 rounded bg-gray-200"></div>
+        <div className="mb-2 h-4 rounded bg-muted"></div>
+        <div className="h-3 rounded bg-muted"></div>
       </div>
       <div className="w-full">
         <div className="grid grid-cols-6 gap-4">
-          <div className="h-20 rounded bg-gray-200"></div>
-          <div className="h-20 rounded bg-gray-200"></div>
-          <div className="h-20 rounded bg-gray-200"></div>
-          <div className="h-20 rounded bg-gray-200"></div>
-          <div className="h-20 rounded bg-gray-200"></div>
-          <div className="h-20 rounded bg-gray-200"></div>
+          <div className="h-20 rounded bg-muted"></div>
+          <div className="h-20 rounded bg-muted"></div>
+          <div className="h-20 rounded bg-muted"></div>
+          <div className="h-20 rounded bg-muted"></div>
+          <div className="h-20 rounded bg-muted"></div>
+          <div className="h-20 rounded bg-muted"></div>
         </div>
         <div className="mt-4">
           <div className="mb-3 flex items-center text-sm">
-            <div className="h-3 w-1/3 rounded bg-gray-200"></div>
+            <div className="h-3 w-1/3 rounded bg-muted"></div>
           </div>
-          <div className="h-10 rounded bg-gray-200"></div>
+          <div className="h-10 rounded bg-muted"></div>
         </div>
       </div>
     </main>
