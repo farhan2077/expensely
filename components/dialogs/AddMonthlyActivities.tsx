884bfdb49a248616e13151a4e9cfcad94600f461
farhan2077 <farhanbinamin@gmail.com>
2024-08-30 17:01:35 +0600

    fix: use custom ScrollArea without focus trap (ScrollArea from shadcn/ui was causing problem with tab order)

--- components/dialogs/AddMonthlyActivities.tsx
+++ components/dialogs/AddMonthlyActivities.tsx
@@ -11,8 +11,8 @@ import { z } from "zod";
 import { addMonthlyActivity } from "@/app/actions/monthly-activity";
 
 import { Icons } from "@/components/icons";
+import ScrollAreaNoFocus from "@/components/scroll-area-no-focus";
 import { Button } from "@/components/ui/button";
-// import { format } from "date-fns";
 import {
   Dialog,
   DialogContent,
@@ -29,7 +29,6 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { ScrollArea } from "@/components/ui/scroll-area";
 
 import { monthlyActivityFormSchema } from "@/libs/validations/monthly-activity";
 
@@ -113,98 +112,99 @@ export default function AddMonthlyActivities({
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent
-        // p-6 is applied by default, but <ScrollArea/> was cutting the input field ring thus p-5 here and additional p-1 is applied later
-        className="max-w-lg overflow-hidden p-5"
+        // <DialogContent /> has p-6 by default
+        // but <ScrollAreaNoFocus /> is cutting the input field ring due to overflow-hidden, so p-2 here (scrollbar will also be present inside this p-2)
+        // and additional p-4 is applied later, in p-4 this scrollbar is placed so it is not placed on top of the content on the dialog
+        className="max-w-lg overflow-hidden p-2"
       >
-        <ScrollArea className="max-h-[85vh]">
-          <div className="p-1">
-            <DialogHeader className="mb-4">
-              <DialogTitle>Add info</DialogTitle>
-              <DialogDescription>
-                Add group members&apos; rent and paid amount
-              </DialogDescription>
-            </DialogHeader>
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)}>
-                <div className="divide-y-2 divide-muted *:pb-4 *:pt-3 first:*:pt-0 last:*:pb-0">
-                  {fields.map((field, index) => (
-                    <div className="grid grid-cols-1" key={field.id}>
-                      <div className="grid grid-cols-12 gap-4">
-                        <div className="col-start-1 col-end-5">
-                          <p className="mt-1 flex items-center text-ellipsis text-sm font-medium leading-none">
-                            <span className="mr-1.5 inline-flex h-4 w-4 items-center justify-center rounded bg-muted-foreground/20 text-xs tabular-nums text-black/80">
-                              {index + 1}
-                            </span>
-                            <span>{groupMembers[index].user.name}</span>
-                          </p>
-                        </div>
-                        <FormField
-                          control={form.control}
-                          name={`formGroups.${index}.rent`}
-                          render={({ field }) => (
-                            <FormItem className="col-start-5 col-end-9 space-y-1">
-                              <FormLabel>Rent</FormLabel>
-                              <FormControl>
-                                <Input
-                                  className="placeholder:tracking-tight"
-                                  type="number"
-                                  inputMode="numeric" // display numeric keyboard on mobile
-                                  placeholder="Rent amount"
-                                  autoComplete="rent"
-                                  {...field}
-                                />
-                              </FormControl>
-                              <FormMessage className="text-xs" />
-                            </FormItem>
-                          )}
-                        />
-                        <FormField
-                          control={form.control}
-                          name={`formGroups.${index}.paid`}
-                          render={({ field }) => (
-                            <FormItem className="col-start-9 col-end-13 space-y-1">
-                              <FormLabel>Paid</FormLabel>
-                              <FormControl>
-                                <Input
-                                  className="placeholder:tracking-tight"
-                                  type="number"
-                                  inputMode="numeric" // display numeric keyboard on mobile
-                                  placeholder="Paid amount"
-                                  autoComplete="paid"
-                                  {...field}
-                                />
-                              </FormControl>
-                              <FormMessage className="text-xs" />
-                            </FormItem>
-                          )}
-                        />
+        {/* <ScrollArea /> from shadcn/ui or radix-ui is not used here due to focus trapping issues, instead custom one is used here which is without focus trap */}
+        <ScrollAreaNoFocus className="max-h-[85vh] overflow-y-auto p-4">
+          <DialogHeader className="mb-4">
+            <DialogTitle>Add info</DialogTitle>
+            <DialogDescription>
+              Add group members&apos; rent and paid amount
+            </DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="divide-y-2 divide-muted *:pb-4 *:pt-3 first:*:pt-0 last:*:pb-0">
+                {fields.map((field, index) => (
+                  <div className="grid grid-cols-1" key={field.id}>
+                    <div className="grid grid-cols-12 gap-4">
+                      <div className="col-start-1 col-end-5">
+                        <p className="mt-1 flex items-center text-ellipsis text-sm font-medium leading-none">
+                          <span className="mr-1.5 inline-flex h-4 w-4 items-center justify-center rounded bg-muted-foreground/20 text-xs tabular-nums text-black/80">
+                            {index + 1}
+                          </span>
+                          <span>{groupMembers[index].user.name}</span>
+                        </p>
                       </div>
+                      <FormField
+                        control={form.control}
+                        name={`formGroups.${index}.rent`}
+                        render={({ field }) => (
+                          <FormItem className="col-start-5 col-end-9 space-y-1">
+                            <FormLabel>Rent</FormLabel>
+                            <FormControl>
+                              <Input
+                                className="placeholder:tracking-tight"
+                                type="number"
+                                inputMode="numeric" // display numeric keyboard on mobile
+                                placeholder="Rent amount"
+                                autoComplete="rent"
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage className="text-xs" />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name={`formGroups.${index}.paid`}
+                        render={({ field }) => (
+                          <FormItem className="col-start-9 col-end-13 space-y-1">
+                            <FormLabel>Paid</FormLabel>
+                            <FormControl>
+                              <Input
+                                className="placeholder:tracking-tight"
+                                type="number"
+                                inputMode="numeric" // display numeric keyboard on mobile
+                                placeholder="Paid amount"
+                                autoComplete="paid"
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage className="text-xs" />
+                          </FormItem>
+                        )}
+                      />
                     </div>
-                  ))}
-                </div>
+                  </div>
+                ))}
+              </div>
 
-                <div className="mt-4 flex gap-4">
-                  <Button
-                    variant={"outline"}
-                    className="w-full"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      fillEmptyFields();
-                    }}
-                  >
-                    Fill empty fields
-                  </Button>
-                  <Button disabled={isLoading} type="submit" className="w-full">
-                    {isLoading && (
-                      <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-                    )}
-                    {isLoading ? "" : "Add monthly data"}
-                  </Button>
-                </div>
-              </form>
-            </Form>
-          </div>
-        </ScrollArea>
+              <div className="mt-4 flex gap-4">
+                <Button
+                  variant={"outline"}
+                  className="w-full"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    fillEmptyFields();
+                  }}
+                >
+                  Fill empty fields
+                </Button>
+                <Button disabled={isLoading} type="submit" className="w-full">
+                  {isLoading && (
+                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                  {isLoading ? "" : "Add monthly data"}
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </ScrollAreaNoFocus>
       </DialogContent>
     </Dialog>
   );
