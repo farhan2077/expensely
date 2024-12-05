921572849587f1342279ca4f6aad1741644ded28
farhan2077 <farhanbinamin@gmail.com>
2024-12-06 02:48:13 +0600

    new: reduce sentry build size

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,23 @@ const nextConfig = {
       },
     ];
   },
+  webpack: (config, { webpack, isServer }) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __SENTRY_DEBUG__: false,
+        __SENTRY_TRACING__: false,
+        __RRWEB_EXCLUDE_IFRAME__: true,
+        __RRWEB_EXCLUDE_SHADOW_DOM__: true,
+        __SENTRY_EXCLUDE_REPLAY_WORKER__: true,
+      })
+    );
+    if (isServer) {
+      config.devtool = "source-map";
+    }
+
+    // Important: return the modified config
+    return config;
+  },
 };
 
 const bundleAnalyzer = withBundleAnalyzer({
